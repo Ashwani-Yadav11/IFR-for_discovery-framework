88dffcb27fcc2701f96619ffca0215b4c1433d35
abhishekkeshri10 <abhishek.keshri@salesforce.com>
2022-06-07 16:24:56 +0530

    LWC UI changes

--- IFR Discovery Framework/force-app/main/default/lwc/mainComponent/mainComponent.js
+++ IFR Discovery Framework/force-app/main/default/lwc/mainComponent/mainComponent.js
@@ -1,3 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class MainComponet extends LightningElement {}
+export default class MainComponent extends LightningElement {}
---
+++ IFR Discovery Framework/force-app/main/default/lwc/textListViewer/textListViewer.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class TextListViewer extends LightningElement {}
