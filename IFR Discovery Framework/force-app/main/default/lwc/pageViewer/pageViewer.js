755487d180c2bed20aa4c4dbd13a635eb465e859
abhishekkeshri10 <abhishek.keshri@salesforce.com>
2022-06-07 17:59:20 +0530

    LWC component to show pdf

--- IFR Discovery Framework/force-app/main/default/lwc/pageViewer/pageViewer.js
+++ IFR Discovery Framework/force-app/main/default/lwc/pageViewer/pageViewer.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
-export default class PageViewer extends LightningElement {}
+export default class PageViewer extends LightningElement {
+    @api fileId;
+    @api heightInRem;
+
+    get pdfHeight() {
+        return 'height: ' + this.heightInRem + 'rem';
+    }
+    
+    get url() {
+        return '/sfc/servlet.shepherd/version/renditionDownload?rendition=THUMB720BY480&versionId=068RM0000001kDMYAY';
+    }
+}
