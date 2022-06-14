65e37c03f6c33a253924029b1c961d8b8dbef5e4
abhishekkeshri10 <abhishek.keshri@salesforce.com>
2022-06-14 23:52:56 +0530

    Ui changes

--- IFR Discovery Framework/force-app/main/default/lwc/pageViewer/pageViewer.js
+++ IFR Discovery Framework/force-app/main/default/lwc/pageViewer/pageViewer.js
@@ -3,12 +3,15 @@ import { NavigationMixin } from 'lightning/navigation';
 
 export default class PageViewer extends NavigationMixin(LightningElement)  {
     @api contentDocumentId='';
+    @api heightInRem='';
     //contentDocumentId='069RM0000001hmjYAA';
     get pdfHeight() {
         return 'height: ' + this.heightInRem + 'rem';
     }
     recordPageUrl;
-
+    get url() {
+        return '/sfc/servlet.shepherd/version/renditionDownload?rendition=JPGZ&versionId=068RM0000001kDMYAY';
+    }
     connectedCallback() {
         // Generate a URL to a User record page
         this[NavigationMixin.GenerateUrl]({
