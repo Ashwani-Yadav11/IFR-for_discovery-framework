179a507c0eae94f88d6c40ecc4f7052de1beb3c7
Ashwani Yadav <ashwani.yadav@salesforce.com>
2022-06-15 12:29:38 +0530

    Drag List Component

--- IFR Discovery Framework/force-app/main/default/lwc/draggableComponent/draggableComponent.js
+++ IFR Discovery Framework/force-app/main/default/lwc/draggableComponent/draggableComponent.js
@@ -5,18 +5,193 @@ export default class Draggercomponent extends LightningElement {
     accounts={
         data:[
             {
-                Name:'Abhishek',
+                Name:'Name',
                 id:'1',
                 
             },
             {
-                Name:'Ashwani',
+                Name:'Gender',
                 id:'2',
                 
-            }
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },
+            {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },
+            {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },     {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },     {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },
+            {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },
+            {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },
+            {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },     {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+                
+            },     {
+                Name:'Name',
+                id:'1',
+                
+            },
+            {
+                Name:'Gender',
+                id:'2',
+                
+            },
+            { Name:'Address',
+                id:'3',
+                
+            },
+            { Name:'Account Number',
+                id:'3',
+        }
         ]
     };
-    handleDragStart(event){
-        event.dataTransfer.setData("account_id",event.target.dataset.accountid);
+    selectValue = '';
+    handleDragStart(e){
+        e.dataTransfer.setData("account_id",e.target.dataset.accountname);
+      console.log(e.target.dataset.accountname+' dragged');
+      
+    }
+
+    createTemplate(e){
+        
+    
+        console.log('A question template with '+e.target.dataset.accountname+' as Question Text field will be created');
+     
     }
 }
