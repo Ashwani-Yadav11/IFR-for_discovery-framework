ecca1955aa36573f45b8318147fbe9a546bba1c3
abhishekkeshri10 <abhishek.keshri@salesforce.com>
2022-06-21 12:13:18 +0530

    Main component changes

--- IFR Discovery Framework/force-app/main/default/lwc/mainComponent/mainComponent.js
+++ IFR Discovery Framework/force-app/main/default/lwc/mainComponent/mainComponent.js
@@ -1,10 +1,26 @@
-import { LightningElement,track } from 'lwc';
+import returnExtractionStatus from '@salesforce/apex/GetExtractedData.returnExtractionStatus';
+import { LightningElement,track, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 
 export default class MainComponent extends LightningElement {
     fileUploaded=false;
      contentId;
-    @track contentDocumentI='';
+    spinner=false;
+    success=false;
+    
+    @track contentDocumentId='';
     @track contentVersionId;
+    @wire(returnExtractionStatus,{contentDocumentId:'$contentDocumentId'})
+    successStatus(data,error){
+        if(data==true)
+        {
+            this.success = true;
+            this.spinner=false;
+        }
+        console.log(data);
+        console.log(error) ;
+        console.log(this.successStatus,'SuccessStatus');
+    }
     handleCustomEvent(event)
     {
         this.contentId = event.detail;
@@ -14,6 +30,12 @@ export default class MainComponent extends LightningElement {
             this.contentVersionId = ids[0];
         console.log(event.detail,'maincomp');
         this.fileUploaded = true;
+        this.spinner=true;
+        checkExtractionStatus();
     }
+    checkExtractionStatus(){
+        setInterval(refreshApex(this.successStatus),5000);
+    }
+
     
 }
