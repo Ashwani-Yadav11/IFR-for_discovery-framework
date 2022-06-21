c2a7c4376197481c58916a4ab24c467bac7a11da
abhishekkeshri10 <abhishek.keshri@salesforce.com>
2022-06-21 19:39:30 +0530

    Working Code

--- IFR Discovery Framework/force-app/main/default/lwc/draggableComponent/draggableComponent.js
+++ IFR Discovery Framework/force-app/main/default/lwc/draggableComponent/draggableComponent.js
@@ -1,29 +1,47 @@
 import startRenditionAndExtractText from '@salesforce/apex/ProcessDocument.startRenditionAndExtractText';
 import returnExtractedTexts from '@salesforce/apex/GetExtractedData.returnExtractedTexts';
 
-import { LightningElement,wire ,api} from 'lwc';
+import { LightningElement,wire ,api,track} from 'lwc';
 //import getAccounts from '@salesforce/apex/AccountController.getAccounts';
 export default class Draggercomponent extends LightningElement {
     //@wire(getAccounts) accounts;
 
 
 
-  @api contentDocumentId;
+ @api contentDocumentId;
  //contentDocumentId='069RM0000001hyBYAQ';
  
-
+    @track listOfTexts=[];
     @wire(returnExtractedTexts,{contentDocumentId:'$contentDocumentId'})
     texts({data,error}){
-        console.log(data,'Extracted Text');
+   console.log(this.contentDocumentId);
+        console.log(data,this.contentDocumentId+' Extracted Text');
+        if(data)
+        {
+            let pages = data.ocrDocumentScanResults;
+        for(let i = 0;i<pages.length;i++)
+        {
+            //for
+            let keyValue=[];
+            keyValue = pages[i].keyValuePairs;
+           
+            for(let j=0;j<keyValue.length;j++)
+            {
+                let tex = keyValue[j].key.text;
+                this.listOfTexts.push({
+                    text:tex,
+                    id:this.listOfTexts.length+1
+                });
+                console.log(tex);
+            }
+            
+
+        }
+        console.log(this.listOfTexts,'ListOftexts');
+    }
         console.log(error,'Extracted Text Error');
     }
     
-
-
-
-    
-    
-   
     accounts={
         data:[
             {
