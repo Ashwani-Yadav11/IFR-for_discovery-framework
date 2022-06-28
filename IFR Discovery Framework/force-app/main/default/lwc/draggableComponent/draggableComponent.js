fa8d6ec800e29ce68249a2229cfd4f20c79f18fe
Ashwani Yadav <ashwani.yadav@salesforce.com>
2022-06-28 12:22:31 +0530

    SortList

--- IFR Discovery Framework/force-app/main/default/lwc/draggableComponent/draggableComponent.js
+++ IFR Discovery Framework/force-app/main/default/lwc/draggableComponent/draggableComponent.js
@@ -60,7 +60,7 @@ export default class Draggercomponent extends LightningElement {
             if(this.selectedOption=='Sort a-z'){
                 this.listOfTexts.sort((text1, text2) => {
                 let compareText1=text1.text.toLowerCase();
-                let compareText2=text2.toLowerCase();
+                let compareText2=text2.text.toLowerCase();
                 if (compareText1< compareText2) {
                     return -1;
                 }
@@ -73,11 +73,11 @@ export default class Draggercomponent extends LightningElement {
             if(this.selectedOption=='Sort z-a'){
                 this.listOfTexts.sort((text1, text2) => {
                 let compareText1=text1.text.toLowerCase();
-                let compareText2=text2.toLowerCase();
+                let compareText2=text2.text.toLowerCase();
                 if (compareText1< compareText2) {
                     return 1;
                 }
-                if (compareText1> compareText) {
+                if (compareText1> compareText2) {
                     return -1;
                 }
                 return 0;
