40f43e03ab24c4d6aeff5d73a3f734e769890283
abhishekkeshri10 <abhishek.keshri@salesforce.com>
2022-07-04 13:55:57 +0530

    landingComponent

--- IFR Discovery Framework/force-app/main/default/lwc/landingComponent/landingComponent.js
+++ IFR Discovery Framework/force-app/main/default/lwc/landingComponent/landingComponent.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+// //------------------------------------------------------------------------------
+// //======================= Imports =================================
+// //------------------------------------------------------------------------------
+import { LightningElement, track, wire } from "lwc";
+import { CurrentPageReference } from "lightning/navigation";
 
-export default class LandingComponent extends LightningElement {}
+export default class LandingComponent extends LightningElement {
+  @track emailSucessRedirection = false;
+  @track contentDocumentId = "";
+  currentPageReference = null;
+  urlStateParameters = null;
+  /**
+   * @method getStateParameters : this is a wire method to get the url parameter
+   * @param {CurrentPageRefernce} currentPageReference
+   */
+  @wire(CurrentPageReference)
+  getStateParameters(currentPageReference) {
+    if (currentPageReference) {
+      this.urlStateParameters = currentPageReference.state;
+      this.setParametersBasedOnUrl();
+    }
+  }
+  /**
+   * @method setParametersBasedOnUrl : extracts the contentDocumentId from url
+   * url form is ..?c__contentDocumentId=<contentDocumentId> if it is a emailRedirected url
+   */
+  setParametersBasedOnUrl() {
+    this.contentDocumentId =
+      this.urlStateParameters.c__contentDocumentId || null;
+    if (this.contentDocumentId != null) {
+      this.emailSucessRedirection = true;
+    }
+  }
+}
